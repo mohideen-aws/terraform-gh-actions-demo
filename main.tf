c7fb7ebce1c0cfc32e38555f0599419787ca3ac4
shethkevalku <105679652+shethkevalku@users.noreply.github.com>
2022-05-19 12:53:32 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # Require TF version to be same as or greater than 0.12.13
 terraform {
-  required_version = ">=0.12.13"
-  #backend "s3" {
-  #  bucket         = "kyler-github-actions-demo-terraform-tfstate"
-  #  key            = "terraform.tfstate"
-  #  region         = "us-east-1"
-  #  dynamodb_table = "aws-locks"
-  #  encrypt        = true
-  #}
+  required_version = "1.1.9"
+  backend "s3" {
+    bucket         = "kyler-github-actions-demo-terraform-tfstate"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "aws-locks"
+    encrypt        = true
+  }
 }
 
 # Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
@@ -29,6 +29,7 @@ module "bootstrap" {
   aws_iam_policy_permits_name = "GitHubActionsIamPolicyPermits"
   aws_iam_policy_assume_name  = "GitHubActionsIamPolicyAssume"
 }
+*/
 
 # Build the VPC
 resource "aws_vpc" "vpc" {
@@ -60,4 +61,4 @@ resource "aws_route_table" "route_table2" {
     Terraform = "true"
   }
 }
-*/
+
