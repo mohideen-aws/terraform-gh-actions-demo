4f439c9e4144bcc2a911a0a433431446943f2a1f
shethkevalku <105679652+shethkevalku@users.noreply.github.com>
2022-05-19 13:06:07 +0100

    creating s3 and dynamodb

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # Require TF version to be same as or greater than 0.12.13
 terraform {
-  required_version = "1.1.9"
-  backend "s3" {
-    bucket         = "myterraformgithubactions3bucket1234555"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "aws-locks-dynamodb-githubactionskeval"
-    encrypt        = true
-  }
+  required_version = ">=0.12.13"
+  #backend "s3" {
+    #bucket         = "myterraformgithubactions3bucket1234555"
+    #key            = "terraform.tfstate"
+    #region         = "us-east-1"
+    #dynamodb_table = "aws-locks-dynamodb-githubactionskeval"
+    #encrypt        = true
+  #}
 }
 
 # Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
@@ -17,20 +17,21 @@ provider "aws" {
 }
 
 
-/* Commented out until after bootstrap
+# Commented out until after bootstrap
 
 # Call the seed_module to build our ADO seed info
 module "bootstrap" {
   source                      = "./modules/bootstrap"
-  name_of_s3_bucket           = "kyler-github-actions-demo-terraform-tfstate"
-  dynamo_db_table_name        = "aws-locks"
+  name_of_s3_bucket           = "meratests3bucketskeval-128907654678"
+  dynamo_db_table_name        = "aws-locks0-meratests3bucketskeval"
   iam_user_name               = "GitHubActionsIamUser"
   ado_iam_role_name           = "GitHubActionsIamRole"
   aws_iam_policy_permits_name = "GitHubActionsIamPolicyPermits"
   aws_iam_policy_assume_name  = "GitHubActionsIamPolicyAssume"
 }
-*/
 
+
+/*
 # Build the VPC
 resource "aws_vpc" "vpc" {
   cidr_block           = "10.1.0.0/16"
@@ -61,4 +62,5 @@ resource "aws_route_table" "route_table2" {
     Terraform = "true"
   }
 }
+  */
 
