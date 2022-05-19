c3fad7c9e0ce1eb6fa99e8186feb9df55ecd6dcd
shethkevalku <105679652+shethkevalku@users.noreply.github.com>
2022-05-19 15:23:12 +0100

    sdadasdas

--- main.tf
+++ main.tf
@@ -157,7 +157,7 @@ create_before_destroy = true
 } #security group ends here
 
 resource "aws_instance" "ec2" {
- ami                    = "ami-0b418580298265d5c"
+ ami                    = "ami-005de95e8ff495156"
  instance_type          = "t2.micro"
  subnet_id              = aws_subnet.my_subnet.id
  vpc_security_group_ids = [aws_security_group.allow_web.id]
